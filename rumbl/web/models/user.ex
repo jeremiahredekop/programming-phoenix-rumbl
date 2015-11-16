c152b89ccc34bb5d109534a8196860341c4ae0d4
Jeremiah Redekop <jeremiah@geniuscode.net>
2015-11-15 21:59:16 -0800

    Building Forms

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -7,4 +7,10 @@ defmodule Rumbl.User do
     field :password_hash, :string
     timestamps
   end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
